c5be58bfa8731b1a65622f728d9ed2e9320234e6
kim-yongdae <ydkim2110@gmail.com>
2021-04-20 14:10:31 +0900

    파트12. 플레이리스트 상세화면

---
+++ Spotify Clone/ViewModels/PlaylistheaderViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistheaderViewModel.swift
+//  Spotify Clone
+//
+//  Created by 김용대 on 2021/04/20.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
