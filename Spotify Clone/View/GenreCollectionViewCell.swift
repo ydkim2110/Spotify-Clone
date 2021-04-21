0b876b7caebf4e5d90b5178df1e7186c18ed5523
kim-yongdae <ydkim2110@gmail.com>
2021-04-21 12:09:04 +0900

    파트14. 서치 화면 구현

---
+++ Spotify Clone/View/GenreCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  GenreCollectionViewCell.swift
+//  Spotify Clone
+//
+//  Created by 김용대 on 2021/04/21.
+//
+
+import UIKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+    static let identifier = "GenreCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .white
+        imageView.image = UIImage(systemName: "music.quarternote.3", withConfiguration: UIImage.SymbolConfiguration(pointSize: 50, weight: .regular))
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        return label
+    }()
+    
+    private let colors: [UIColor] = [
+        .systemPink,
+        .systemBlue,
+        .systemPurple,
+        .systemOrange,
+        .systemGreen,
+        .systemRed,
+        .systemGray,
+        .systemYellow,
+        .darkGray,
+        .systemTeal
+    ]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        contentView.addSubview(label)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.text = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        label.frame = CGRect(x: 10, y: contentView.height / 2, width: contentView.width - 20, height: contentView.height / 2)
+        imageView.frame = CGRect(x: contentView.width / 2, y: 0, width: contentView.width / 2, height: contentView.height / 2)
+    }
+    
+    func configure(with title: String) {
+        label.text = title
+        contentView.backgroundColor = colors.randomElement()
+    }
+}
